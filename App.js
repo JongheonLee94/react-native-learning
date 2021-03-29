a2f1fc1f0d5d4080a2fc4be2f1428d86c8871157
Jongheon <ismir1449@gmail.com>
2021-03-30 02:10:47 +0900

    First commit

---
+++ App.js
@@ -0,0 +1,80 @@
+import { StatusBar } from 'expo-status-bar';
+import React ,{Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class MyComponent extends Component {
+  constructor(props) {
+
+      super(props);
+      this.state = {
+          year: 2020,
+          name: 'lee',
+          colors: ['blue'],
+          book:'props'
+      }
+      this.updateYear = this.updateYear.bind(this);
+      // this.update = this.update.bind(this);
+      this.updateBook = this.updateBook.bind(this);
+  }
+  updateYear(){
+      this.setState({ //setState가 끝에 render를 호출하여 화면이 바뀌게 된다.
+          year: 2021
+      })
+      
+  }
+  update() {
+    this.state.year=2019 //직접 바꾸게 되면 render가 호출되지 않아 화면이 바뀌지 않는다.
+    this.forceUpdate()   //추천되지는 않지만 화면을 강제로 업데이트해서 변경사항을 적용한다. 
+  }
+  updateBook(){
+    this.setState({
+      book:'changed props'
+    })
+  }
+render(){
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app! Hello world</Text>
+      
+                <Text>My name is {this.state.name}</Text>
+                <Text 
+                  onPress={() => this.updateYear() } >
+                    The year is {this.state.year}
+                </Text>
+                <Text>My colors are {this.state.colors[0]}</Text>
+                <Text 
+                onPress={ () => this.update()}>
+                  Force Update
+                </Text>
+                <Text onPress={() => this.updateBook()}>
+                { this.state.book }</Text>
+                <BookDisplay updateBook={() => this.updateBook() } 
+                book={this.state.book}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+}
+
+class BookDisplay extends Component {
+  render() {
+    return(
+      <View>
+        <Text onPress={ () => this.props.updateBook()}>
+        { this.props.book }</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+export default MyComponent
